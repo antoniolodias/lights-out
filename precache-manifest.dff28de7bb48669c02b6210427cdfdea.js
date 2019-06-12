5213368dc0a1e126675276286bc9326004eb3c90
Antonio Dias <antoniolodias@gmail.com>
2019-06-12 18:44:13 +0200

    Updates

---
+++ precache-manifest.dff28de7bb48669c02b6210427cdfdea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121690909d689e541029beccd76c52fb",
+    "url": "/lights-out/index.html"
+  },
+  {
+    "revision": "4f799396f5a4b13c0319",
+    "url": "/lights-out/static/css/main.3ce49b5d.chunk.css"
+  },
+  {
+    "revision": "511a6eb6b7ec2e1e4ec3",
+    "url": "/lights-out/static/js/2.26ba37d3.chunk.js"
+  },
+  {
+    "revision": "4f799396f5a4b13c0319",
+    "url": "/lights-out/static/js/main.5b401385.chunk.js"
+  },
+  {
+    "revision": "d6b7daafe64bb82a9237",
+    "url": "/lights-out/static/js/runtime~main.69e4817a.js"
+  }
+]);
